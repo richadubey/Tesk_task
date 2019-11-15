6ff06a1304bcba71a27e1503311105ea4556e41b
richadubey <richadubey39@gmail.com>
2019-11-16 02:29:50 +0530

    remove_extra_models

--- db/schema.rb
+++ db/schema.rb
@@ -20,14 +20,6 @@ ActiveRecord::Schema.define(version: 2019_11_15_200223) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "authentications", force: :cascade do |t|
-    t.string "provider"
-    t.string "uid"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "calendars", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -68,17 +60,6 @@ ActiveRecord::Schema.define(version: 2019_11_15_200223) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "roles", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.string "resource_type"
-    t.bigint "resource_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
-    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -100,23 +81,12 @@ ActiveRecord::Schema.define(version: 2019_11_15_200223) do
     t.datetime "updated_at", null: false
     t.bigint "college_id"
     t.string "username"
-    t.string "provider"
-    t.string "uid"
-    t.string "gender"
     t.boolean "is_admin"
     t.index ["college_id"], name: "index_users_on_college_id"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
-  end
-
-  create_table "users_roles", id: false, force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "role_id"
-    t.index ["role_id"], name: "index_users_roles_on_role_id"
-    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
-    t.index ["user_id"], name: "index_users_roles_on_user_id"
   end
 
 end
